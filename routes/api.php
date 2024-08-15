8540d19bc3e13b014910b734f67919877e5470a9
Patrick Bandeira <patrick010509@gmail.com>
2024-08-15 14:57:58 -0300

    feat (api): hello world

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function (){
+    return response()->json([
+        'message' => 'Hello API',
+    ], 200);
+});
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
